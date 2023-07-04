ed54a5e6e9d57a9a1710f97d61226ba143c48cb4
ulvelx <ulyanaspr@mail.ru>
2023-07-04 16:49:03 +0300

    New version

---
+++ src/client.cpp
@@ -0,0 +1,65 @@
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <boost/asio.hpp>
+
+using boost::asio::ip::tcp;
+
+enum { max_length = 1024 };
+
+int main(int argc, char* argv[])
+{
+    try
+    {
+
+        if (argc != 3) //в начале задаются аргументы командной строки: хост и порт, с учётом нулевого аргумента - путь файла, получается 3 аргумента, если не равно трём, то значит не заданы какие-то параметры, или задано слишком много
+        {
+            std::cerr << "Usage: blocking_tcp_echo_client <host> <port>\n";
+            return 1;
+        }
+
+        size_t numOfThreads, numOfCycles;
+        std::cout << "Please enter the number of threads: ";
+        std::cin >> numOfThreads;
+        std::cout << "Please enter the number of cycles: ";
+        std::cin >> numOfCycles;
+
+        //cin игнорирует символы пробела и оставляет их в потоке как мусор, поэтому нужно проионорировать этот мусор
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+        char request[max_length] = "";
+        char reply[max_length] = "";
+
+        for (int i = 0; i < numOfThreads; ++i) { //запросы каждого клиента
+
+            // одно подключение для одного клиента
+            boost::asio::io_context io_context;
+
+            tcp::socket s(io_context);
+            tcp::resolver resolver(io_context); //объект resolver, который исользуется для разрешения имени хоста и порта
+            boost::asio::connect(s, resolver.resolve(argv[1], argv[2])); //устаналивает соединение с удалённым сервером, используя разрешённые значения хоста и порта
+
+            for (int j = 0; j < numOfCycles; ++j) { //запросы одного единственного клиента в данный момент сессии
+                std::cout << "Enter message: ";
+
+                std::cin.getline(request, max_length); //ввод сообщения от пользователя
+                size_t request_length = std::strlen(request);
+                boost::asio::write(s, boost::asio::buffer(request, request_length)); //запись сообщения в буфер для передачи данных 
+
+
+                size_t reply_length = boost::asio::read(s,
+                    boost::asio::buffer(reply, request_length)); //ответ от сервера, который считывается из сокета 
+                std::cout << "Reply is: ";
+                std::cout.write(reply, reply_length);
+                std::cout << "\n";
+            }
+
+        }
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << "Exception: " << e.what() << "\n";
+    }
+
+    return 0;
+}
---
+++ src/server.cpp
@@ -0,0 +1,117 @@
+#include <cstdlib>
+#include <iostream>
+#include <memory>
+#include <utility>
+#include <vector>
+#include <boost/asio.hpp>
+
+using boost::asio::ip::tcp;
+
+class session //сессия обмена данными с клиентом
+    : public std::enable_shared_from_this<session>
+{
+public:
+    session(tcp::socket socket, size_t connectedClient) //конструктор принимает сокет (IP, Port)
+        : socket_(std::move(socket)), connectedClient_(connectedClient)
+    {
+    }
+
+    void start() //метод запускает асинхронное чтение
+    {
+        do_read(); //асинхронное чтение
+    }
+
+private:
+    void do_read()
+    {
+        auto self(shared_from_this());
+        socket_.async_read_some(boost::asio::buffer(data_, max_length), //читаем в буфер, захватываем в лямбду
+            //обработчика завершающий указатель на разделяемую копию себя, 
+            [this, self](boost::system::error_code ec, std::size_t length)
+            {
+                if (!ec)
+                {
+                    do_write(length); //асинхронная запись данных 
+                }
+                else {
+                    std::cout << "connection lost " << connectedClient_ << std::endl;
+                }
+            });
+    }
+
+    void do_write(std::size_t length)
+    {
+        auto self(shared_from_this());
+        boost::asio::async_write(socket_, boost::asio::buffer(data_, length), //асинхронная запись данныз в сокет
+            [this, self](boost::system::error_code ec, std::size_t /*length*/)
+            {
+                if (!ec) //после успешной записи снова идёт чтение из сокета
+                {
+                    do_read();
+                }
+                else {
+                    std::cout << "connection lost " << connectedClient_ << std::endl;
+                }
+            });
+    }
+
+    tcp::socket socket_;
+    size_t connectedClient_;
+    enum { max_length = 1024 };
+    char data_[max_length];
+};
+
+class server
+{
+public:
+    server(boost::asio::io_context& io_context, short port)
+        : acceptor_(io_context, tcp::endpoint(tcp::v4(), port)), //объект ассептор, который используется для прослушивания подключений на указанном порту
+        connectedClient(1)
+    {
+        do_accept(); //асинхронное принимает входящее подключение на указ. порту
+    }
+
+private:
+    void do_accept()
+    {
+        acceptor_.async_accept(
+            [this](boost::system::error_code ec, tcp::socket socket)
+            {
+                if (!ec) //если подключение принято успешно, создаётся новый объект сессии и начинается обмен данными с клиентом 
+                {
+                    std::cout << "connection established: " << connectedClient << std::endl;
+                    std::make_shared<session>(std::move(socket), connectedClient)->start();
+                    ++connectedClient;
+                }
+
+                do_accept(); //вызывается снова, чтобы ожидать следующего подключения
+            });
+    }
+
+    size_t connectedClient;
+    tcp::acceptor acceptor_;
+};
+
+int main(int argc, char* argv[])
+{
+    try
+    {
+        if (argc != 2) //аргумент командной строки - порт
+        {
+            std::cerr << "Usage: async_tcp_echo_server <port>\n";
+            return 1;
+        }
+
+        boost::asio::io_context io_context;
+
+        server s(io_context, std::atoi(argv[1])); //передаётся порт
+
+        io_context.run(); //метод run объекта io_context запускает цикл обработки событий, который продолжается, пока есть активные асинхронные операции
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << "Exception: " << e.what() << "\n";
+    }
+
+    return 0;
+}
